abec26e2683a232dc39543db03ba18e90d141ac1
RenaatHaleydt <renaat.haleydt@gmail.com>
2023-11-23 07:46:09 +0100

    Adding basic navigation to detail page

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,15 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/f/library'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, fioriLibrary) {
         "use strict";
 
         return Controller.extend("ap.customerapplication.controller.Main", {
             onInit: function () {
 
+            },
+
+            onListItemPress: function () {
+                var oFCL = this.oView.getParent().getParent();
+
+                oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
             }
         });
     });
