8d0dd2403212b9575ff673fd21e53f0a2e8e423e
RenaatHaleydt <renaat.haleydt@gmail.com>
2023-11-24 08:43:36 +0100

    Adding sorting functionality on customers table

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,22 +1,65 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    'sap/f/library'
+    'sap/ui/Device',
+    'sap/f/library',
+    'sap/ui/model/Sorter',
+    'sap/ui/core/Fragment',
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, fioriLibrary) {
+    function (Controller, Device, fioriLibrary, Sorter, Fragment) {
         "use strict";
 
         return Controller.extend("ap.customerapplication.controller.Main", {
             onInit: function () {
-
+                // Keeps reference to any of the created sap.m.ViewSettingsDialog-s in this sample
+			    this._mViewSettingsDialogs = {};
             },
 
             onListItemPress: function () {
                 var oFCL = this.oView.getParent().getParent();
 
                 oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
-            }
+            },
+            handleSortButtonPressed: function () {
+                this.getViewSettingsDialog("ap.customerapplication.fragments.sortDialog")
+                    .then(function (oViewSettingsDialog) {
+                        oViewSettingsDialog.open();
+                    });
+            },
+            getViewSettingsDialog: function (sDialogFragmentName) {
+                var pDialog = this._mViewSettingsDialogs[sDialogFragmentName];
+    
+                if (!pDialog) {
+                    pDialog = Fragment.load({
+                        id: this.getView().getId(),
+                        name: sDialogFragmentName,
+                        controller: this
+                    }).then(function (oDialog) {
+                        if (Device.system.desktop) {
+                            oDialog.addStyleClass("sapUiSizeCompact");
+                        }
+                        return oDialog;
+                    });
+                    this._mViewSettingsDialogs[sDialogFragmentName] = pDialog;
+                }
+                return pDialog;
+            },
+            handleSortDialogConfirm: function (oEvent) {
+                var oTable = this.byId("customersTable"),
+                    mParams = oEvent.getParameters(),
+                    oBinding = oTable.getBinding("items"),
+                    sPath,
+                    bDescending,
+                    aSorters = [];
+    
+                sPath = mParams.sortItem.getKey();
+                bDescending = mParams.sortDescending;
+                aSorters.push(new Sorter(sPath, bDescending));
+    
+                // apply the selected sort and group settings
+                oBinding.sort(aSorters);
+            },
         });
     });
